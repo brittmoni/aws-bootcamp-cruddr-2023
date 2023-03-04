88be607755d0cbf14d551e80056de260c1132af1
Brittany F <brittfranklin92@gmail.com>
2023-03-04 03:59:38 +0000

    Starting rollbar; making updates to week 2 journal

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -2,6 +2,10 @@ from flask import Flask
 from flask import request
 from flask_cors import CORS, cross_origin
 import os
+# Rollbar
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
 
 from services.home_activities import *
 from services.notifications_activities import *
@@ -33,6 +37,24 @@ provider.add_span_processor(processor)
 simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
 provider.add_span_processor(simple_processor)
 
+# Rollbar
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
@@ -51,6 +73,11 @@ cors = CORS(
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
+
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
--- journal/week2.md
+++ journal/week2.md
@@ -1,1 +1,3 @@
 # Week 2 — Distributed Tracing
+
+For this week's homework, I updated the app.py file in the backend-flask directory with the instructions in order to utilize Honeycomb for observability of the Cruddur back-end. This was done by reviewing the Honeycomb documentation. 
