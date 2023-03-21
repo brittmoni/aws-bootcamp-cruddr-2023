03fd38d49f7cf74d70e3a9631decf4de4c315ffb
Brittany F <brittfranklin92@gmail.com>
2023-03-21 02:31:27 +0000

    readding psycopg setup files

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -6,9 +6,10 @@ from lib.db import pool
 tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
-  def run(logger):
-    logger.info('Hello Cloudwatch! from  /api/activities/home')    
-    with tracer.start_as_current_span("home-activities-mock-data"):
+  def run(cognito_user_id=None):
+    print("HOME ACTIVITY")
+    #logger.info("HomeActivities")
+    with tracer.start_as_current_span("home-activites-mock-data"):
       span = trace.get_current_span()
       now = datetime.now(timezone.utc).astimezone()
       span.set_attribute("app.now", now.isoformat())
@@ -17,6 +18,23 @@ class HomeActivities:
       SELECT * FROM activities
       """
 
+      # sql = query_wrap_array("""
+      # SELECT
+      #   activities.uuid,
+      #   users.display_name,
+      #   users.handle,
+      #   activities.message,
+      #   activities.replies_count,
+      #   activities.reposts_count,
+      #   activities.likes_count,
+      #   activities.reply_to_activity_uuid,
+      #   activities.expires_at,
+      #   activities.created_at
+      # FROM public.activities
+      # LEFT JOIN public.users ON users.uuid = activities.user_uuid
+      # ORDER BY activities.created_at DESC
+      # """)
+      # print(sql)
       with pool.connection() as conn:
         with conn.cursor() as cur:
           cur.execute(sql)
@@ -24,5 +42,4 @@ class HomeActivities:
           # the first field being the data
           json = cur.fetchall()
       return json[0]
-
       return results
