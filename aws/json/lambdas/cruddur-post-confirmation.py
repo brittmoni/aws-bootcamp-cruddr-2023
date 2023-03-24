7f2598f8bc9b5b1593afa3c595841775f2c7d70d
Brittany F <brittfranklin92@gmail.com>
2023-03-24 03:13:44 +0000

    adding lambda post confirmation

---
+++ aws/json/lambdas/cruddur-post-confirmation.py
@@ -0,0 +1,43 @@
+import json
+import psycopg2
+import os
+
+def lambda_handler(event, context):
+    user = event['request']['userAttributes']
+    print("User Attributes")
+    print(user)
+
+    user_display_name = user['name']
+    user_handle = user['preferred_username']
+    user_email = user['email']
+    user_cognito_user_id = user['sub']
+
+    try:
+        print("Entered try")
+        conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
+        cur = conn.cursor()
+
+        sql = f"""
+            INSERT INTO public.users (display_name, handle, email, cognito_user_id) 
+            VALUES('{user_display_name}', '{user_handle}', '{user_email}', '{user_cognito_user_id}')
+        """
+
+        print("SQL Statement")
+        print(sql)
+
+        conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
+        cur = conn.cursor()
+
+        cur.execute(sql)
+        conn.commit() 
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        
+    finally:
+        if conn is not None:
+            cur.close()
+            conn.close()
+            print('Database connection closed.')
+
+    return event
