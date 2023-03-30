25b26316968d0052361760063f0068bb489bea7f
Brittany Franklin <brittfranklin92@gmail.com>
2023-03-30 10:19:36 -0400

    One more sql file that was updated for testing purposes

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -3,10 +3,12 @@ VALUES
   ('Andrew Brown', 'andrewbrown' ,'MOCK'),
   ('Andrew Bayko', 'bayko' ,'MOCK');
 
-INSERT INTO public.activities (user_uuid, message, expires_at)
+INSERT INTO public.activities (uuid, message, expires_at)
 VALUES
   (
     (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
+
+  -- In line 6, uuid was originally user_uuid
