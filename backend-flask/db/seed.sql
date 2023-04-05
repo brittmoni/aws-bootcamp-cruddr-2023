56ef8216c9e66ed5ebd0341be7dbc1124a737e64
Brittany F <brittfranklin92@gmail.com>
2023-04-05 03:10:21 +0000

    Creating scripts for dynamodb and updating dp.py function

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,12 +1,12 @@
-INSERT INTO public.users (display_name, handle, cognito_user_id)
+INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'test@email.com', 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko', 'test@email.com', 'bayko' ,'MOCK');
+  ('Andrew Brown', 'test@email.com', 'andrewbrown' , 'MOCK'),
+  ('Andrew Bayko', 'test@email.com', 'bayko' , 'MOCK');
 
-INSERT INTO public.activities (uuid, message, expires_at)
+INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT user_uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -27,15 +27,15 @@ class Db:
     connection_url = os.getenv("CONNECTION_URL")
     self.pool = ConnectionPool(connection_url)
 
-  def print_sql(self, title, sql):
+  def print_sql(self, title, sql, params={}):
     cyan = '\033[96m'
     no_color = '\033[0m'
     print(f'{cyan} SQL STATEMENT-[{title}]------{no_color}')
-    print(sql)
+    print(sql,params)
 
 # Check for returning in all caps
   def query_commit(self,sql,params={}):
-    self.print_sql('commit with returning',sql)
+    self.print_sql('commit with returning', sql, params)
 
     pattern = r"\bRETURNING\b"
     is_returning_id = re.search(pattern, sql)
@@ -64,9 +64,19 @@ class Db:
     #   except Exception as err:
     #     print_sql_err(err)
 
+# when we want to return a a single value
+  def query_value(self,sql,params={}):
+    self.print_sql('value', sql, params)
+
+    with self.pool.connection() as conn:
+      with conn.cursor() as cur:
+        cur.execute(sql,params)
+        json = cur.fetchone()
+        return json[0]
+
   # return json object
   def query_object(self, sql, parameters={}):
-    self.print_sql('json', sql)
+    self.print_sql('json', sql, params)
     print("SQL Statement - [object] ----------")
     print(sql)
     wrapped_sql = self.query_wrap_object(sql)
@@ -80,7 +90,7 @@ class Db:
 
   # return array of json objects
   def query_array(self, sql, parameters={}):
-    self.print_sql('array', sql)
+    self.print_sql('array', sql, params)
     print("SQL Statement - [array] ----------")
     print(sql)
 
