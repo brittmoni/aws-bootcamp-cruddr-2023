fce228306237c551d0931d8af5292c882b9c9c9e
Brittany F <brittfranklin92@gmail.com>
2023-04-28 01:44:43 +0000

    week 5 semi complete, need to continue working on

--- backend-flask/services/create_message.py
+++ backend-flask/services/create_message.py
@@ -43,7 +43,7 @@ class CreateMessage:
         rev_handle = ''
       else:
         rev_handle = user_receiver_handle
-      users = db.query_array_json(sql,{
+      users = db.query_array(sql,{
         'cognito_user_id': cognito_user_id,
         'user_receiver_handle': rev_handle
       })
